2de8df3469c2825a18093b4a48be40e48f0c992b
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-09-12 19:29:02 -0300

    Removed Buffer (nodejs) code

--- src/codec.js
+++ src/codec.js
@@ -4,5 +4,5 @@ module.exports = {}
 
 module.exports.FlateDecode = {
   encode: (data) => pako.deflate(data),
-  decode: (stream) => Buffer.from(pako.inflate(stream)).toString('UTF8')
+  decode: (stream) => pako.inflate(stream, { to: 'string' })
 }
